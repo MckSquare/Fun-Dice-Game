fe0e0b134e1c52817f38b0c202982e321d963d8f
Koustav Mallick <24458780+MckSquare@users.noreply.github.com>
2017-08-10 22:32:31 +0530

    files that make you feel happy

---
+++ dice.py
@@ -0,0 +1,23 @@
+import random as rnd
+c=rnd.randint(1,6)
+count=0
+life=0
+flag=0
+print ("Welcome to my dice game!\n Here all you have to do is guess a number\nAnd your PC will do the rest.\n")
+print ("Rules: You have only 3 lives. You get wrong, you fail.\n You get right, score's given 5 for each success!\nEnjoy :p")
+print ("Enter your guess:")
+
+while(flag!=1):
+	n=int(input())
+	if n<1 or n>6:
+		print ("Give a number between [1,6].")
+	elif n==c:
+	    count+=1
+	    print("You got a correct answer!")
+	elif n!=c:
+		life+=1
+		print("Answer's wrong! Life left", 3-life)
+
+	if life==3:
+		flag=1
+		print("Well, better luck next time!")
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Download iff you've python installed :p
+this is just a fun game, and one of my early creations.
+please download dice2 if you've python 2 installed (This code is not tested, will appreciate if you got any correction)
+dice.py is only for python 3 users.
+
+to use it, best way: open cmd in the download folder, run python dice.py (or python dice2.py #for python 2 users)
+
+Thank you!
